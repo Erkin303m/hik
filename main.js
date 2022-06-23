0ebc18bbf5624aa644d4202d265e190988af315b
Erkin303m <maxmudjonoverkin189@gmail.com>
2022-06-23 17:52:28 +0500

    asdasd

---
+++ main.js
@@ -0,0 +1,84 @@
+// if(bu yerga shart yoziladi){
+// agar shart bajarilsa shu yer ishlaydi
+// } else{
+// agar bajarilmasa bu yer ishlaydi
+// }
+
+// let erkin= 24;
+// let azizulloh=20;
+
+
+// if (erkin > 22 || azizulloh>20){
+//     console.log("hamma kirishi mumkin");
+// }else{
+//     console.log("kirmasin");
+// }
+
+
+
+
+// let vaqt=50;
+
+// if(vaqt > 8 && vaqt <= 24){
+//     console.log("kirmaysan ishgaaaaa");
+// } 
+// else if(vaqt>0 && vaqt<=8){
+//     console.log("malades");
+// } else{
+//     console.log("bunday vaqt mavjud emas");
+// }
+
+
+
+
+
+// let yosh = parseInt(prompt("Istalgan yoshni kiriting"));
+
+// if (yosh >= 1 && yosh <= 3) {
+//     console.log("chaqaloq");
+// } else if (yosh >= 4 && yosh <= 7) {
+//     console.log("godak");
+// } else if (yosh >= 8 && yosh <= 12) {
+//     alert("bola")
+// } else if (yosh >= 13 && yosh <= 17) {
+//     console.log("osmir");
+// } else if (yosh >= 18 && yosh <= 20) {
+//     console.log("ospirin");
+// }
+
+
+
+
+
+
+
+
+// for(let i=20; i<=100; i++){
+//     console.log("salom" , i+10);
+// }
+
+// let a = -5, b = -5, c = -6;
+// let musbatsonlar = 0;
+
+// if (a > 0) {
+//     musbatsonlar++;
+// }
+
+// if (b > 0) {
+//     musbatsonlar++
+// }
+
+// if (c > 0) {
+//     musbatsonlar++
+// }
+
+
+// console.log("barcha musbat sonlar", musbatsonlar)
+
+
+
+
+
+for (let i = 1.2; i <=2; i=i+0.2) {
+    console.log((i)*10500);
+}
